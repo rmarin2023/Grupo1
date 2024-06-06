113970e9b071f97a663b0b13a2988c19fd3d2439
Abraham Expósito García <169030329+aexpositog06@users.noreply.github.com>
2024-06-06 13:04:18 +0200

    Update presentacion.java

--- presentacion.java
+++ presentacion.java
@@ -5,21 +5,7 @@ public class presentacion
 {
 	public static void main(String[] args) {
 		System.out.println("Hola mi nombre es Juanma Vizcaíno!");
-	}
-}
-
-/* HelloWorld.java
- */
-
-public class presentacion
-{
-	public static void main(String[] args) {
 		System.out.println("Hola mi nombre es Jesús!");
-	}
-}
-public class presentacion
-{
-	public static void main(String[] args) {
 		System.out.println("Hola mi nombre es Abraham Expósito García!");
 	}
 }
