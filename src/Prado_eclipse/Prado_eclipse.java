11dd4ea13b29daa090743f3db0921af4de968da1
user <user@eclipse>
2024-06-06 13:25:35 +0200

    mi archivo

---
+++ src/Prado_eclipse/Prado_eclipse.java
@@ -0,0 +1,10 @@
+package Prado_eclipse;
+
+public class Prado_eclipse {
+
+	public static void main(String[] args) {
+		// TODO Esbozo de método generado automáticamente
+
+	}
+
+}
---
+++ src/module-info.java
@@ -0,0 +1,8 @@
+/**
+ * 
+ */
+/**
+ * 
+ */
+module Prado_eclipse {
+}
