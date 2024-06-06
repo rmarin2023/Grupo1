44bf6aaece4063d9a1bb38c275a0f056b29b1857
Jesús Blázquez <jesuspesquina@gmail.com>
2024-06-06 10:57:48 +0000

    Commit jesus

--- presentacion.java
+++ presentacion.java
@@ -7,3 +7,13 @@ public class presentacion
 		System.out.println("Hola mi nombre es Juanma Vizcaíno!");
 	}
 }
+
+/* HelloWorld.java
+ */
+
+public class presentacion
+{
+	public static void main(String[] args) {
+		System.out.println("Hola mi nombre es Jesús!");
+	}
+}
