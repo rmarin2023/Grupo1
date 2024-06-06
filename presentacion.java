6dc48c48e7b7a348a511b1bc5db438b62e82d11b
Manuel Cerro <169028408+manucerro@users.noreply.github.com>
2024-06-06 13:07:52 +0200

    Update presentacion.java

--- presentacion.java
+++ presentacion.java
@@ -17,3 +17,10 @@ public class presentacion
 		System.out.println("Hola mi nombre es Jesús!");
 	}
 }
+
+public class presentacion
+{
+	public static void main(String[] args) {
+		System.out.println("Hola mi nombre es Manuel Cerro!");
+	}
+}
