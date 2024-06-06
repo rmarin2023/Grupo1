27e533c627e4632e54e3412d4ba4b55272f49711
Manuel Cerro <169028408+manucerro@users.noreply.github.com>
2024-06-06 13:10:27 +0200

    Update presentacion.java

--- presentacion.java
+++ presentacion.java
@@ -1,22 +1,3 @@
-/* HelloWorld.java
- */
-
-public class presentacion
-{
-	public static void main(String[] args) {
-		System.out.println("Hola mi nombre es Juanma Vizcaíno!");
-	}
-}
-
-/* HelloWorld.java
- */
-
-public class presentacion
-{
-	public static void main(String[] args) {
-		System.out.println("Hola mi nombre es Jesús!");
-	}
-}
 
 public class presentacion
 {
